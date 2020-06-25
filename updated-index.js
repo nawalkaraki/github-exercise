5023bdf808ccca37d504c1bb24322b32757d2c45
nawal karaki <nawalkaraki79@gmail.com>
2020-06-25 22:54:52 +0300

    > Created updated-index.js and wrote some comments in it >

--- updated-index.js
+++ updated-index.js
@@ -1,5 +1,5 @@
 // write the console.log code with hello in it underneath
-
+console.log("hello Nawal");
 
 // write the console.log code with goodbye in it underneath
-
+console.log("Goodbyea Nawal");
